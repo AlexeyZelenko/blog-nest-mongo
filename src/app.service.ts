9f5a449aa4777852fe4dd24a39a5299cf45cd2ca
AlexeyZelenko <zelenkooleksii75@gmail.com>
2021-01-13 15:06:11 +0200

    start 3

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PRODUCTS } from './mock/products.mock';
 
-// @Injectable()
-// export class AppService {
-//   getHello(): string {
-//     return 'Hello World!';
-//   }
-// }
 @Injectable()
 export class AppService {
-    products = PRODUCTS;
-
-    async getProducts() {
-        return await this.products;
-    }
+  getHello(): string {
+    return 'Hello World!';
+  }
 }
