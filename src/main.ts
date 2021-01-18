91d9e94769a2444dde3a089d04f3d3ff1a6bba0a
AlexeyZelenko <zelenkooleksii75@gmail.com>
2021-01-18 11:02:44 +0200

    start 4

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(port, host, function () {
+        console.log('Server started.......')
+  });
 }
 bootstrap();
